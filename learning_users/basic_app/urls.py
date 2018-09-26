98c88a4c7205d42ce8fc68e734c71153b29b8ed6
LAPTOP-7C5H1RJ1\Cevher <efeozden@gmail.com>
2018-09-26 10:09:15 +0300

    first

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,9 @@
+from basic_app import views
+from django.urls import path
+
+#TEMPLATE URLS!
+app_name= 'basic_app'
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login'),
+]
